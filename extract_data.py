32fd465800e8e65e0b157a0b29c4599f0efc8851
StianJakobsen <stian.mjelde20@hotmail.com>
2020-03-18 17:06:51 +0100

    extract data bitsjes

--- extract_data.py
+++ extract_data.py
@@ -1,30 +1,48 @@
 import pandas as pd
 import numpy as np
+import sklearn
+from sklearn import preprocessing, linear_model
+from sklearn.linear_model import SGDClassifier
 
 
 def extract_data():
     data = pd.read_csv("iris.data", sep=",")
+    le = preprocessing.LabelEncoder()
     x1 = list(data["sepal_length"])
     x2 = list(data["sepal_width"])
     x3 = list(data["petal_length"])
     x4 = list(data["petal_width"])
-    y = list(data["class"])
-    return x1, x2, x3, x4, y
+    y = list(le.fit_transform(list(data["class"])))
+    return list(zip(x1, x2, x3, x4)), y
 
 
-def get_training_set(x1, x2, x3, x4, y):
-    x1_train = x1[0:30] + x1[50:80] + x1[100:130]
-    x2_train = x2[0:30] + x2[50:80] + x2[100:130]
-    x3_train = x3[0:30] + x3[50:80] + x3[100:130]
-    x4_train = x4[0:30] + x4[50:80] + x4[100:130]
-    y_train = y[0:30] + y[50:80] + y[100:130]
-    return x1_train, x2_train, x3_train, x4_train, y_train
+def get_sets():
+    x, y = extract_data()
+    clc1_x_train, clc1_x_test, clc1_y_train, clc1_y_test = sklearn.model_selection.train_test_split(x[0:50], y[0:50],
+                                                                                                    test_size=0.4)
+    clc2_x_train, clc2_x_test, clc2_y_train, clc2_y_test = sklearn.model_selection.train_test_split(x[50:100], y[50:100],
+                                                                                                    test_size=0.4)
+    clc3_x_train, clc3_x_test, clc3_y_train, clc3_y_test = sklearn.model_selection.train_test_split(x[100:150], y[100:150],
+                                                                                                    test_size=0.4)
+    x_train = clc1_x_train + clc2_x_train + clc3_x_train
+    x_test = clc1_x_test + clc2_x_test + clc3_x_test
+    y_train = clc1_y_train + clc2_y_train + clc3_y_train
+    y_test = clc1_y_test + clc2_y_test + clc3_y_test
+    return  x_train, x_test, y_train, y_test
 
 
-def get_test_set(x1, x2, x3, x4, y):
-    x1_test = x1[30:50] + x1[80:100] + x1[130:150]
-    x2_test = x2[30:50] + x2[80:100] + x2[130:150]
-    x3_test = x3[30:50] + x3[80:100] + x3[130:150]
-    x4_test = x4[30:50] + x4[80:100] + x4[130:150]
-    y_test = y[30:50] + y[80:100] + y[130:150]
-    return x1_test, x2_test, x3_test, x4_test, y_test
+def confusion_matrix(prediction, actual, numClasses):
+    confusionMatrix = np.zeros((numClasses,numClasses))
+    for i in range(len(prediction)):
+        confusionMatrix[actual[i]][prediction[i]] += 1
+    return confusionMatrix
+
+def main():
+    x_train, x_test, y_train, y_test = get_sets()
+    clf = linear_model.SGDClassifier(max_iter=1000, tol=1e-3, alpha=0.00001)
+    clf.fit(x_train, y_train)
+    pred = clf.predict(x_test)
+    actual = y_test
+    print(confusion_matrix(pred,actual,3))
+
+main()
