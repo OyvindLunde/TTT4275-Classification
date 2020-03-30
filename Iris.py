9e681f5b53a0cded6d568ecd002075d06bd71015
oyvindrl <oylunrl@gmail.com>
2020-03-30 12:39:35 +0200

    Some changes

--- Iris.py
+++ Iris.py
@@ -2,9 +2,9 @@ import extract_data as ed
 import math_functions as mf
 
 def main():
-    x_train, x_test, y_train, y_test = ed.get_sets()
-    print(x_train)
-    W = mf.calculate_W(100,0.001, y_train, x_train)
-    print(W)
+    x_train, x_test, y_train, y_test, t = ed.get_sets()
+    #print(x_train)
+    W = mf.calculate_W(100,0.001, t, x_train)
+    #print(W)
 
 main()
--- math_functions.py
+++ math_functions.py
@@ -19,8 +19,6 @@ def calculate_MSE(W, t, x):
 #print(calculate_MSE(test, test2, x))
 
 def calculate_g(W,x):
-    print(len(W))
-    print(len(W[0]))
     z = np.dot(W,x)
     #print(len(z[0]))
     #print(z)
@@ -40,13 +38,11 @@ def calculate_MSE_gradient(W, t, x):
     g_gradient = g*(1-g)
     z_gradient = x.T
 
-    return np.dot(MSE_gradient,g_gradient*z_gradient)
+    return np.dot(MSE_gradient,np.dot(g_gradient,z_gradient))
 
 #print(calculate_MSE_gradient(test, test2, x))
 
 def calculate_W(iterations, alpha, t, x):
-    print(len(x))
-    print(len(x[0]))
     W = np.zeros((len(t),len(x)))
     #W = np.ones((len(t),len(x)))
     #print(x)
