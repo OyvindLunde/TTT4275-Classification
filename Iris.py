831c6ccc259b7aa0372c0f4adf66006b5d5fccf0
oyvindrl <oylunrl@gmail.com>
2020-03-30 13:40:18 +0200

    Fixed MSE_grad?

--- Iris.py
+++ Iris.py
@@ -4,7 +4,7 @@ import math_functions as mf
 def main():
     x_train, x_test, y_train, y_test, t = ed.get_sets()
     #print(x_train)
-    W = mf.calculate_W(100,0.001, t, x_train)
-    #print(W)
+    W = mf.calculate_W(1000,0.0001, t, x_train)
+    print(W)
 
 main()
--- math_functions.py
+++ math_functions.py
@@ -1,11 +1,6 @@
 import numpy as np
 import math
 
-test = np.array([[1,2,3,4,5], [4,5,6,1,1], [7,8,9,4,3]]) # testing values, completely trash
-test2 = np.array([[4,5,6], [7,8,9], [1,4,5], [9,8,10]])
-test2 = np.transpose(test2)
-x = np.array([[1,1,1,1,1],[5,5,5,5,5],[2,2,2,2,2],[9,9,9,9,9]])
-x = np.transpose(x)
 
 def calculate_MSE(W, t, x):
     g = W.dot(x)
@@ -16,36 +11,36 @@ def calculate_MSE(W, t, x):
 
     return sum/2
 
-#print(calculate_MSE(test, test2, x))
-
 def calculate_g(W,x):
     z = np.dot(W,x)
-    #print(len(z[0]))
-    #print(z)
     g = np.zeros((len(z),len(z[0])))
-    #print(g)
     for i in range(len(z)):
         for k in range(len(z[0])):
             g[i][k] = 1/(1+math.exp(-z[i][k]))
-    #print(g)
     return g
 
-#print(calculate_g(test,x))
+def matrixMul(x,y):
+    z = np.zeros((len(x),len(y)))
+
+    for i in range(len(y)):
+        for j in range(len(x)):
+            z[j][i] = y[i]*x[j]
+
+    return z
 
 def calculate_MSE_gradient(W, t, x):
     g = calculate_g(W,x)
-    MSE_gradient = g-t
-    g_gradient = g*(1-g)
-    z_gradient = x.T
+    sum = np.zeros((3,4))
+    for i in range(len(g)):
+        diff = g[:,i]-t[:,i]
+        scalar_prod = np.dot(g[:,i],(1-g[:,i]))
+        sum += matrixMul(np.dot(diff,scalar_prod), x[:,i])
+    return sum
 
-    return np.dot(MSE_gradient,np.dot(g_gradient,z_gradient))
-
-#print(calculate_MSE_gradient(test, test2, x))
+    return np.dot(MSE_gradient, temp)
 
 def calculate_W(iterations, alpha, t, x):
     W = np.zeros((len(t),len(x)))
-    #W = np.ones((len(t),len(x)))
-    #print(x)
     for i in range(iterations):
         W = W - alpha*calculate_MSE_gradient(W,t,x)
 
