0c6207b325d7aebd30b9325fe0cbc65238c250cc
StianJakobsen <stian.mjelde20@hotmail.com>
2020-04-08 12:45:38 +0200

    Implemented data extracting functions

--- handwritten_numbers_task/src/extract_data.py
+++ handwritten_numbers_task/src/extract_data.py
@@ -8,5 +8,14 @@ def read_idx(filename):
         return np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
 
 
+train_set = read_idx("../number_data/MNist_ttt4275/test_images.bin")
+labels = read_idx("../number_data/MNist_ttt4275/train_labels.bin")
+
+def get_sets():
+    train_set = read_idx("../number_data/MNist_ttt4275/train_images.bin")
+    train_labels = read_idx("../number_data/MNist_ttt4275/train_labels.bin")
+    test_set = read_idx("../number_data/MNist_ttt4275/test_images.bin")
+    test_labels = read_idx("../number_data/MNist_ttt4275/test_labels.bin")
+    return train_set, train_labels, test_set, test_labels
 
 
